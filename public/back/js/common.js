cdb66432cbf09d1ddfaea77a0fd923559db883b9
Harley <harley1993@qq.com>
2018-05-10 10:25:33 +0800

    公共进度条功能完成

---
+++ public/back/js/common.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Harley on 2018/05/10.
+ */
+
+//禁用进度环
+NProgress.configure({showSpinner: false});
+
+/*
+* 1.ajaxComplete  请求完成的时候调用（不管成功失败）
+*
+* 2.ajaxSuccess   请求成功时调用
+* 3.ajaxError     请求失败的时候调用
+*
+* 4.ajaxStart     ajax开始发送的时候调用（只会调用一次）
+*
+* 5.ajaxSend      请求发送的时候调用
+*
+*
+* 6.ajaxStop      ajax停止的时候调用
+*
+* */
+
+$(document).ajaxStart(function () {
+  //开启进度条
+  NProgress.start();
+})
+
+$(document).ajaxStop(function () {
+  setTimeout(function () {
+    //结束进度条
+    NProgress.done();
+  }, 500);
+})
+
+
+
+
+
