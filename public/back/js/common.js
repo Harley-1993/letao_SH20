243b3588797e1689cd63992fcc43c9c4a6f68fc3
Harley <harley1993@qq.com>
2018-05-10 15:47:47 +0800

    左侧菜单和二级菜单切换效果

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -6,19 +6,19 @@
 NProgress.configure({showSpinner: false});
 
 /*
-* 1.ajaxComplete  请求完成的时候调用（不管成功失败）
-*
-* 2.ajaxSuccess   请求成功时调用
-* 3.ajaxError     请求失败的时候调用
-*
-* 4.ajaxStart     ajax开始发送的时候调用（只会调用一次）
-*
-* 5.ajaxSend      请求发送的时候调用
-*
-*
-* 6.ajaxStop      ajax停止的时候调用
-*
-* */
+ * 1.ajaxComplete  请求完成的时候调用（不管成功失败）
+ *
+ * 2.ajaxSuccess   请求成功时调用
+ * 3.ajaxError     请求失败的时候调用
+ *
+ * 4.ajaxStart     ajax开始发送的时候调用（只会调用一次）
+ *
+ * 5.ajaxSend      请求发送的时候调用
+ *
+ *
+ * 6.ajaxStop      ajax停止的时候调用
+ *
+ * */
 
 $(document).ajaxStart(function () {
   //开启进度条
@@ -30,7 +30,25 @@ $(document).ajaxStop(function () {
     //结束进度条
     NProgress.done();
   }, 500);
+});
+
+$(function () {
+  
+  $('.category').click(function () {
+    
+    $('.lt_aside .child').stop().slideToggle();
+  })
+  
+  //
+  $('.icon_menu').click(function () {
+    
+    $('.lt_aside').toggleClass("hidemenu");
+    $('.lt_topbar').toggleClass("hidemenu");
+    $('.lt_main').toggleClass("hidemenu");
+  })
 })
+
+
 
 
 
